cfe2a031608ed53714405047cad76a88d40c2dca
SergiyPysanenko <SergiyPysanenko@gmail.com>
2023-09-19 20:37:49 +0300

    add hw 02

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,25 +1,18 @@
 const express = require('express')
-
 const router = express.Router()
 
-router.get('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+const contactsController = require("../../controllers");
+const { validateData, checkBody } = require('../../helpers');
 
-router.get('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.get('/', contactsController.getContacts);
 
-router.post('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.get('/:contactId', contactsController.getContact)
 
-router.delete('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.post('/', validateData, contactsController.addContact);
 
-router.put('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.delete('/:contactId', contactsController.deleteContact);
+
+router.put('/:contactId', checkBody, validateData, contactsController.updateContact);
+
 
 module.exports = router
