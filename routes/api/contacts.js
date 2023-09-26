5df781dc3b601ed5a40ac9dc20b922c0adc7b31a
SergiyPysanenko <SergiyPysanenko@gmail.com>
2023-09-26 20:56:12 +0300

    Fix

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -2,7 +2,8 @@ const express = require('express')
 const router = express.Router()
 
 const contactsController = require("../../controllers");
-const { validateData, checkBody } = require('../../helpers');
+const { validateData } = require('../../helpers');
+const { getContactById, updateContact } = require('../../models')
 
 router.get('/', contactsController.getContacts);
 
@@ -12,7 +13,32 @@ router.post('/', validateData, contactsController.addContact);
 
 router.delete('/:contactId', contactsController.deleteContact);
 
-router.put('/:contactId', checkBody, validateData, contactsController.updateContact);
+
+router.put("/:contactId", async (req, res, next) => {
+    const contactId = req.params.contactId;
+  
+    const { name, email, phone } = req.body;
+  
+      if (!name && !email && !phone) {
+      res.status(400).json({ message: "Missing fields" });
+      } else { const existingContact = await getContactById(contactId);
+      if (existingContact.length === 0) {
+      res.status(404).json({ message: "Contact not found" });
+      } else { const updatedContact = {
+          ...existingContact[0],
+          name: name || existingContact[0].name,
+          email: email || existingContact[0].email,
+          phone: phone || existingContact[0].phone,
+        };
+
+        await updateContact(contactId, updatedContact);
+  
+        res.json({ message: "Contact updated successfully", contact: updatedContact });
+      }
+    }
+  });
+
+
 
 
 module.exports = router
