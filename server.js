167914cbd9b66f87a931d934f1e87d20375c78e0
SergiyPysanenko <SergiyPysanenko@gmail.com>
2023-09-30 21:15:16 +0300

    add homework 03

--- server.js
+++ server.js
@@ -1,5 +1,29 @@
-const app = require('./app')
+const mongoose = require("mongoose");
+const app = require("./app");
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
-})
+const DB_HOST =
+  "mongodb+srv://sergiypysanenko:2RrndgjXPpUxjpsl@db-contacts.dpdgle0.mongodb.net/db-contacts?retryWrites=true&w=majority";
+
+mongoose.connection.once("open", () => {
+  console.log("Database connection successful");
+});
+
+mongoose.connection.on("error", (err) => {
+  console.log("Database connection error:", err.message);
+});
+
+mongoose
+  .connect(DB_HOST)
+  .then(() => {
+    app.listen(3000, () => {
+      console.log("Server running. Use our API on port: 3000");
+    });
+  })
+  .catch((error) => {
+    console.log(error.message);
+    process.exit(1);
+  });
+
+mongoose.connection.on("error", (err) => {
+  console.log("Database connection error:", err.message);
+});
